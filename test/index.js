c29be45ee36f0b909f76b5359564d31a309c4397
Daniel Ochoa <daniel@poeticsystems.com>
2015-04-22 19:37:29 -0500

    change test vars

--- test/index.js
+++ test/index.js
@@ -3,27 +3,28 @@
 var test      = require('tape');
 var BreweryDb = require('../index');
 
-var key = {apiKey: 'fakeKey'};
+var key = {apiKey: 'someApiKey'};
 
 test('BreweryDb', function(t) {
   t.plan(3);
 
   t.throws(function() {
     new BreweryDb();
-  }, /apiKey property needs to be set/,
+  },
+  /apiKey property needs to be set/,
   'apiKey key/value in parameter object');
 
   t.ok(new BreweryDb(key), 'can be instantiated.');
 
-  t.test('beer', function(tn) {
-    tn.plan(1);
+  t.test('beer', function(t) {
+    t.plan(1);
 
     var client = new BreweryDb(key);
 
     return client.beers({name: 'Corona'}).then(function(res) {
-      tn.ok(res);
+      t.ok(res);
     }).catch(function(err) {
-      tn.fail('Should not catch');
+      t.fail('Should not catch');
     });
   });
 

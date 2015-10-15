7fd2e0f2f49de43e54515aca2ff2e2aa86d15cec
Daniel Ochoa <daniel.ochoa@frogdesign.com>
2015-10-14 20:31:55 -0500

    add test to fetch a beer by name from the surface api.

--- test/brewery-db.js
+++ test/brewery-db.js
@@ -3,6 +3,7 @@
 var test      = require('tape');
 var BreweryDb = require('../index');
 var helpers   = require('./helpers');
+var _         = require('lodash');
 
 /**
  *
@@ -17,7 +18,7 @@ helpers.sepia.filterApiKey();
  *
  */
 
-test('BreweryDb instance', function(t) {
+test('BreweryDb can be instanciated', function(t) {
   t.plan(3);
 
   t.throws(function() {
@@ -27,20 +28,16 @@ test('BreweryDb instance', function(t) {
   var breweryDb = new BreweryDb({apiKey: helpers.apiKey});
   t.ok(breweryDb, 'can be instantiated.');
   t.ok(breweryDb._apiKey, 'key is set');
+});
 
-  /*
-  t.test('beer', function(t) {
-    t.plan(1);
+test('can get a beer by name', function(t) {
+  t.plan(1);
 
-    var client = new BreweryDb(key);
+  var client = new BreweryDb({apiKey: helpers.apiKey});
 
-    return client.beers({name: 'Corona'}).then(function(res) {
-      t.ok(res);
-    }).catch(function(err) {
-      t.fail('Should not catch');
-    });
-  });
-  */
+  return client.beers({name: 'Tecate'}).then(function(res) {
+    t.ok(_.isString(res.getBody('utf-8')), 'body is an object');
+  }).catch(t.fail);
 });
 
 
